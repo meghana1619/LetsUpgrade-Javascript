1425f22723e4d95778c405c6736b1270f3a5c4bc
meghana1619 <57556556+meghana1619@users.noreply.github.com>
2020-07-22 12:51:07 +0530

    day6-q1

---
+++ colour.js
@@ -0,0 +1,9 @@
+
+
+setInterval(
+    function () {
+      var randomColor = Math.floor(Math.random()*16777215).toString(16);
+      document.getElementById('para').style.backgroundColor = "#"+randomColor;
+},1000);
+
+
